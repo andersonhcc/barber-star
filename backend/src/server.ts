1017e35d86798b9d6f4d5d09c6ee4da2f3857e68
andersonhcc <programadorandersonh@gmail.com>
2022-12-25 16:29:55 -0300

    configs initial

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
-
+import 'express-async-errors';
+import cors from 'cors';
 import { router} from './routes';
 
 const app = express();
@@ -7,5 +8,22 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+
+  return res.status(500).json({
+    status: 'Error',
+    message: 'Internal Server Error'
+
+  })
+
+
+})
 
 app.listen(3333, () => console.log('Server on'));
